68aeba1438d1eb9113241409d92dcffcb78dfa76
Krrish Singhal <krrishsinghal62@gmail.com>
2026-01-26 13:20:54 +0530

    Port Changed

--- src/server.js
+++ src/server.js
@@ -29,7 +29,9 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(ENV.PORT, () => {
-  console.log(`Server is running on the port${ENV.PORT} `);
+const PORT = process.env.PORT || ENV.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
   connectToDB();
 });
